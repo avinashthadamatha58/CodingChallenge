5309cfa349d12da63f15b4eecc1fdb68ebd0a0b6
Avinash Thadamatha <athadamatha@appriver.com>
2018-12-13 10:37:19 -0500

    First Commit

---
+++ NexidiaCodingChallenge/NUnitTest.cs
@@ -0,0 +1,244 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexidiaCodingChallenge
+{
+    [TestFixture]
+    class NUnitTest
+    {
+        [Test]
+        public void TestHandingExceptions1()
+        {
+            Exception checkFileNotFoundException = null;
+            string[] path = { "c:\\" };
+            try {
+                Program.HandlingExceptions(path);
+            }
+            catch (Exception ex)
+            {
+                checkFileNotFoundException = ex;
+            }
+            string s =checkFileNotFoundException.Message;
+            Assert.IsNotNull(checkFileNotFoundException); //Exception was handled so it does not throw any exception therefore failing the test
+        }
+
+        [Test]
+        public void TestHandingExceptions2()
+        {
+            Exception checkFileNotFoundException = null;
+            string[] path = { "c:\\file.pdf" };
+            try
+            {
+                Program.HandlingExceptions(path);
+            }
+            catch (Exception ex)
+            {
+                checkFileNotFoundException = ex;
+            }
+            string s = checkFileNotFoundException.Message;
+            Assert.IsNotNull(checkFileNotFoundException); //Condition was handled and printed to the screen, so it does not throw any exception therefore failing the test
+        }
+
+        [Test]
+        public void TestHandingExceptions3()
+        {
+            Exception checkFileNotFoundException = null;
+            string[] path = { "c:\\testcase1.pdf" };
+            try
+            {
+                Program.HandlingExceptions(path);
+            }
+            catch (Exception ex)
+            {
+                checkFileNotFoundException = ex;
+            }
+            string s = checkFileNotFoundException.Message;
+            Assert.IsNotNull(checkFileNotFoundException); //Condition was handled and printed to the screen, so it does not throw any exception therefore failing the test
+        }
+
+        [Test]
+        public void TestHandingExceptions4()
+        {
+            Exception checkFileNotFoundException = null;
+            string[] path = { "c:\\\\file.txt" };
+            try
+            {
+                Program.HandlingExceptions(path);
+            }
+            catch (Exception ex)
+            {
+                checkFileNotFoundException = ex;
+            }
+            string s = checkFileNotFoundException.Message;
+            Assert.IsNotNull(checkFileNotFoundException); //Exception was handled so it does not throw any exception therefore failing the test
+        }
+
+        [Test]
+        public void TestAddingTwosToList1()
+        {
+            List<long> factors = new List<long>();
+           Assert.AreEqual(3,Program.AddTwosToList(12, factors));
+        }
+
+        [Test]
+        public void TestAddingTwosToList2()
+        {
+            List<long> factors = new List<long>();
+            Assert.AreEqual(1, Program.AddTwosToList(1, factors));
+        }
+
+        [Test]
+        public void TestAddingTwosToList3()
+        {
+            Exception test = null;
+            List<long> factors = new List<long>();
+            try
+            {
+                Assert.AreEqual(0, Program.AddTwosToList(0, factors)); // handle zero case again in the while therefore no exception, otherwise - infinite loop
+            }
+            catch(Exception e)
+            {
+                test = e;
+            }
+            Assert.IsNotNull(test);
+            
+            }
+
+        [Test]
+        public void TestAddingTwosToList4()
+        {
+            Exception test = null;
+            List<long> factors = new List<long>();
+            try
+            {
+                Assert.AreEqual(-1999999999999999999, Program.AddTwosToList(-1999999999999999999, factors)); // handle negative number case again in the while therefore no exception
+            }
+            catch (Exception e)
+            {
+                test = e;
+            }
+            Assert.IsNotNull(test);
+
+        }
+
+        [Test]
+        public void TestAddingTwosToList5()
+        {
+            Exception test = null;
+            List<long> factors = new List<long>();
+            try
+            {
+                Assert.AreEqual(1999999999999999999, Program.AddTwosToList(1999999999999999999, factors)); 
+            }
+            catch (Exception e)
+            {
+                test = e;
+            }
+            Assert.IsNotNull(test);
+        }
+
+        [Test]
+        public void TestAddAnotherFactor1()
+        {
+            Exception test = null;
+            List<long> factors = new List<long>();
+            try
+            {
+                Assert.AreEqual(-1999999999999999999, Program.AddOtherFactors(-1999999999999999999, factors)); 
+            }
+            catch (Exception e)
+            {
+                test = e;
+            }
+            Assert.IsNotNull(test);
+        }
+
+        [Test]
+        public void TestAnotherFactor2()
+        {
+            Exception test = null;
+            List<long> factors = new List<long>();
+            try
+            {
+                Assert.AreEqual(3, Program.AddOtherFactors(1999999999999999999, factors)); //Assert fails therefore throws exception
+            }
+            catch (Exception e)
+            {
+                test = e;
+            }
+            Assert.IsNotNull(test);
+        }
+
+        [Test]
+        public void TestAnotherFactor3()
+        {
+            Exception test = null;
+            List<long> factors = new List<long>();
+            try
+            {
+                Assert.AreEqual(1, Program.AddOtherFactors(1, factors));
+            }
+            catch (Exception e)
+            {
+                test = e;
+            }
+            Assert.IsNotNull(test);
+        }
+
+        [Test]
+        public void TestAnotherFactor4()
+        {
+            Exception test = null;
+            List<long> factors = new List<long>();
+            try
+            {
+                Assert.AreEqual(0, Program.AddOtherFactors(0, factors));
+            }
+            catch (Exception e)
+            {
+                test = e;
+            }
+            Assert.IsNotNull(test);
+        }
+
+        [Test]
+        public void TestFactorial1()
+        {
+            List<long> factors = new List<long>();
+            Assert.AreEqual(3, Program.CalculatePrimeFactors(12).Count);
+        }
+
+        [Test]
+        public void TestFactorial2()
+        {
+            List<long> factors = new List<long>();
+            Assert.AreEqual(2, Program.CalculatePrimeFactors(1999999999999999999).Count);
+        }
+
+        [Test]
+        public void TestFactorial3()
+        {
+            List<long> factors = new List<long>();
+            Assert.AreEqual(0, Program.CalculatePrimeFactors(-1999999999999999999).Count); //only if number greater than 0, it stores in the list. Therefore list is empty 
+        }
+
+        [Test]
+        public void TestFactorial4()
+        {
+            List<long> factors = new List<long>();
+            Assert.AreEqual(0, Program.CalculatePrimeFactors(0).Count); //only if number greater than 0, it stores in the list. Therefore list is empty 
+        }
+
+        [Test]
+        public void TestFactorial5()
+        {
+            List<long> factors = new List<long>();
+            Assert.AreEqual(0, Program.CalculatePrimeFactors(1).Count); //only if number greater than 0, it stores in the list. Therefore list is empty 
+        }
+    }
+
+}
---
+++ NexidiaCodingChallenge/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NexidiaCodingChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            HandlingExceptions(args); //ToDO make argument readonly
+        }
+
+        public static void HandlingExceptions(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Assumptions: \n1. Only one argument(File path) is given \n2. The path to a file is a text file \n3) Numbers of size greater than 64 bits are considered as string\n");
+
+                if (!File.Exists(args[0]))
+                {
+                    throw new FileNotFoundException("File is not found, please enter a valid path");
+                }
+                if (!Path.GetExtension(args[0]).Equals(".txt"))
+                {
+                    Console.WriteLine("Please enter a file name with .txt extension");
+                    return;
+                }
+                if (new FileInfo(args[0]).Length == 0)
+                {
+                    Console.WriteLine("File does not contain any data, please enter a different file path");
+                    return;
+                }
+                PrimeFactorsInaFile(args);
+            }
+
+            catch (FileNotFoundException fileNotFound)
+            {
+                Console.WriteLine(fileNotFound.FileName + " " + fileNotFound.Message);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Please provide command line arguments first");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("File error, please try entering a different file");
+            }
+        }
+
+        public static void PrimeFactorsInaFile(string[] args)
+        {
+            string[] textArray = System.IO.File.ReadAllLines(args[0]); //Automatically opens, reads, and closes the file
+            foreach (string Line in textArray)
+            {
+                {                                                    //fileExists, fileformat, FileOpeningorNotToDo, FileContentNull, - check read permission
+                    if (Int64.TryParse(Line, out Int64 parsedVar) && parsedVar > 1)
+                    {
+                        Console.Write(Line + "  -  ");
+                        WritePrimeFactors(CalculatePrimeFactors(parsedNum: parsedVar));
+                    }
+                    else
+                    {
+                        //if (parsedVar <= 0 && parsedVar>= -Int64.MaxValue)
+                        //{
+                        //    Console.WriteLine(Line + "  -  is neither prime nor composite");
+                        //}
+                        //else if (CheckStringForBigNum(Line))
+                        //{
+                        //    Console.WriteLine(Line + "  -  Please enter a smaller number");
+                        //}
+                        //else
+                        Console.WriteLine(Line + "  -  " + "No prime factors");
+                    }
+                }
+            }
+        }
+        //private static bool CheckStringForBigNum(string eachLine)
+        //{
+        //    foreach (char c in eachLine)
+        //    {
+        //        if (c < '0' || c > '9')
+        //            return false;
+        //    }
+        //    return true;
+        //}
+       public static List<long> CalculatePrimeFactors(long parsedNum)
+        {
+            List<long> primeFactorsList = new List<long>();
+
+            parsedNum = AddTwosToList(parsedNum, primeFactorsList);
+            parsedNum = AddOtherFactors(parsedNum, primeFactorsList);
+
+            if (parsedNum > 1) primeFactorsList.Add(parsedNum);       // If num is not 1, then whatever is left is prime.
+
+            return primeFactorsList;
+        }
+
+        public static long AddTwosToList(long parsedNum, List<long> primeFactorsList)
+        {
+            try
+            {
+                while (parsedNum % 2 == 0 && parsedNum > 0)        // Checking if number is divided by 2 and adding 2 to List
+                {
+                    primeFactorsList.Add(2);
+                    parsedNum /= 2;
+                }
+                return parsedNum;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Arguments");
+                return parsedNum;
+            }
+            }
+
+        public static long AddOtherFactors(long parsedNum, List<long> primeFactorsList)
+        {
+            long factor = 3;         // Take out other primes.
+            while (factor * factor <= parsedNum)
+            {
+                if (parsedNum % factor == 0)
+                {
+                    primeFactorsList.Add(factor);         // This is a factor
+                    parsedNum /= factor;
+                }
+                else
+                {
+                    factor += 2;        // Go to next odd number
+                }
+            }
+            return parsedNum;
+        }
+
+
+        public static void WritePrimeFactors(List<long> result)
+        {
+            int count = result.Count;
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (i == result.Count - 1)
+                {
+                    Console.Write(result[i]);
+                }
+                else
+                {
+                    Console.Write(result[i] + ", ");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
